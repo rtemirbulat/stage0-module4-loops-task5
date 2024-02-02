a822da4d8980ae6650d600285059a3227e7082ef
rtemirbulat <equalityisnotequality@gmail.com>
2024-02-03 01:17:03 +0600

    finished

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -1,7 +1,20 @@
 package school.mjc.stage0.loops.task5;
 
+import static java.lang.Math.round;
+
 public class Cross {
     public void printCross(int sideLength) {
+        int middle = (int) round((double) sideLength /2 + 0.5);
 
+        if(sideLength>0 && sideLength%2!=0) {
+            for (int i = 1; i <= sideLength; i++) {
+                for (int j = 1; j <= sideLength; j++) {
+                    if(j==middle || i==middle) System.out.print("8");
+
+                    else System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
     }
 }
